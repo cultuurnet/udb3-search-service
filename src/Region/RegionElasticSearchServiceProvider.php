24a29a6b82a4201f1e1f5014e947ce61e4904d46
Bert Ramakers <bertramakers@me.com>
2017-02-16 10:48:07 +0100

    III-1779: Add service provider for region-related elasticsearch classes.

---
+++ src/Region/RegionElasticSearchServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CultuurNet\UDB3\SearchService\Region;
+
+use CultuurNet\UDB3\Search\ElasticSearch\Region\ElasticSearchRegionSearchService;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class RegionElasticSearchServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @param Application $app
+     */
+    public function register(Application $app)
+    {
+        $app['region_elasticsearch_service'] = $app->share(
+            function (Application $app) {
+                return new ElasticSearchRegionSearchService(
+                    $app['elasticsearch_client'],
+                    new StringLiteral($app['elasticsearch.region.index_name']),
+                    new StringLiteral($app['elasticsearch.region.document_type'])
+                );
+            }
+        );
+    }
+
+    /**
+     * @param Application $app
+     */
+    public function boot(Application $app)
+    {
+    }
+}
