8427fda4b15700264ea2b19047113e1513e544e3
Bert Ramakers <bertramakers@me.com>
2017-04-06 11:32:03 +0200

    III-1954: Bootstrap the percolator offer region service

--- src/Offer/OfferElasticSearchServiceProvider.php
+++ src/Offer/OfferElasticSearchServiceProvider.php
@@ -6,6 +6,8 @@ use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchPagedResultSetFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\ResultSetJsonDocumentTransformer;
 use CultuurNet\UDB3\Search\ElasticSearch\JsonDocumentTransformingPagedResultSetFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\Offer\ElasticSearchOfferSearchService;
+use CultuurNet\UDB3\Search\ElasticSearch\Offer\PercolatorOfferRegionService;
+use CultuurNet\UDB3\SearchService\EmptyOfferRegionService;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 use ValueObjects\StringLiteral\StringLiteral;
@@ -27,6 +29,21 @@ class OfferElasticSearchServiceProvider implements ServiceProviderInterface
                         new ResultSetJsonDocumentTransformer(),
                         new ElasticSearchPagedResultSetFactory()
                     )
+                );
+            }
+        );
+
+        $app['offer_region_service'] = $app->share(
+            function (Application $app) {
+                // Configuration here is tricky. It might seem like we should
+                // use the read alias for the offer index since we're reading
+                // data. But actually we need to use the write alias because we
+                // need to read data that is relevant when (re-)indexing offer
+                // documents (= writing), and the read alias might be outdated
+                // during a migration.
+                return new PercolatorOfferRegionService(
+                    $app['elasticsearch_client'],
+                    new StringLiteral($app['elasticsearch.offer.write_index'])
                 );
             }
         );
