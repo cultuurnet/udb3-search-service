906d800e843aef7f69eb3a3436028fe05e36b4dc
Bert Ramakers <bertramakers@me.com>
2017-05-12 10:44:24 +0200

    III-2095: Update the injected paged collection factory for offers

--- src/Offer/OfferControllerProvider.php
+++ src/Offer/OfferControllerProvider.php
@@ -4,13 +4,17 @@ namespace CultuurNet\UDB3\SearchService\Offer;
 
 use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchDistanceFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\JsonDocument\EmbeddedJsonDocumentTransformer;
+use CultuurNet\UDB3\Search\Http\JsonLdEmbeddingPagedCollectionFactory;
 use CultuurNet\UDB3\Search\Http\NodeAwareFacetTreeNormalizer;
 use CultuurNet\UDB3\Search\Http\OfferSearchController;
 use CultuurNet\UDB3\Search\Http\PagedCollectionFactory;
+use CultuurNet\UDB3\Search\Http\PagedCollectionFactoryInterface;
+use CultuurNet\UDB3\Search\Http\ResultSetMappingPagedCollectionFactory;
 use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use ValueObjects\StringLiteral\StringLiteral;
 
 class OfferControllerProvider implements ControllerProviderInterface
@@ -52,9 +56,7 @@ class OfferControllerProvider implements ControllerProviderInterface
                     $app['elasticsearch_query_string_factory'],
                     new ElasticSearchDistanceFactory(),
                     new NodeAwareFacetTreeNormalizer(),
-                    new PagedCollectionFactory(
-                        new EmbeddedJsonDocumentTransformer($app['http_client'])
-                    )
+                    $app['offer_paged_collection_factory']
                 );
             }
         );
@@ -63,6 +65,49 @@ class OfferControllerProvider implements ControllerProviderInterface
             function (Application $app) {
                 return $app['offer_search_controller_factory'](
                     $app['offer_elasticsearch_service']
+                );
+            }
+        );
+
+        $app['offer_paged_collection_factory'] = $app->share(
+            function (Application $app) {
+                return new ResultSetMappingPagedCollectionFactory();
+            }
+        );
+
+        $app->before(
+            function (Request $request, Application $app) {
+                // Check if the incoming request has an embed parameter.
+                $embed = $request->query->get('embed', null);
+
+                // Don't do anything if the embed parameter is null or an empty
+                // string.
+                if (is_null($embed) || (is_string($embed) && empty($embed))) {
+                    return;
+                }
+
+                // Convert to a boolean.
+                $embed = filter_var($embed, FILTER_VALIDATE_BOOLEAN);
+
+                if (!$embed) {
+                    // Don't do anything if embed is explicitly set to false.
+                    return;
+                }
+
+                // If embed is true, decorate the paged collection factory used
+                // by offer controllers so it fetches the json-ld of all
+                // results.
+                $app->extend(
+                    'offer_paged_collection_factory',
+                    function (
+                        PagedCollectionFactoryInterface $pagedCollectionFactory,
+                        Application $app
+                    ) {
+                        return new JsonLdEmbeddingPagedCollectionFactory(
+                            $pagedCollectionFactory,
+                            $app['http_client']
+                        );
+                    }
                 );
             }
         );
