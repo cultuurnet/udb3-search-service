96b4d68c62ba261afd67b18ca5d84541de59c031
Bert Ramakers <bert@madewithlove.be>
2019-05-24 13:41:17 +0200

    III-2921 Update bootstrapping of offer and organizer controllers

--- src/Organizer/OrganizerControllerProvider.php
+++ src/Organizer/OrganizerControllerProvider.php
@@ -2,6 +2,8 @@
 
 namespace CultuurNet\UDB3\SearchService\Organizer;
 
+use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\CompositeOrganizerRequestParser;
+use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\WorkflowStatusOrganizerRequestParser;
 use CultuurNet\UDB3\Search\Http\OrganizerSearchController;
 use Silex\Application;
 use Silex\ControllerCollection;
@@ -13,11 +15,15 @@ class OrganizerControllerProvider implements ControllerProviderInterface
     {
         $app['organizer_search_controller'] = $app->share(
             function (Application $app) {
+                $requestParser = (new CompositeOrganizerRequestParser())
+                    ->withParser(new WorkflowStatusOrganizerRequestParser());
+
                 return new OrganizerSearchController(
                     $app['organizer_elasticsearch_query_builder'],
                     $app['organizer_elasticsearch_service'],
-                    $app['paged_collection_factory'],
-                    $app['elasticsearch_query_string_factory']
+                    $requestParser,
+                    $app['elasticsearch_query_string_factory'],
+                    $app['paged_collection_factory']
                 );
             }
         );
