0bc49469c0d66181fe49db86377f3dcdca2997de
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-12 14:54:11 +0200

    Use assertion trait that is compatible with namespaced TestCase

--- tests/ElasticSearch/Organizer/OrganizerJsonDocumentTransformerTest.php
+++ tests/ElasticSearch/Organizer/OrganizerJsonDocumentTransformerTest.php
@@ -5,7 +5,7 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Organizer;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
 use CultuurNet\UDB3\Search\ElasticSearch\PathEndIdUrlParser;
 use CultuurNet\UDB3\Search\ElasticSearch\SimpleArrayLogger;
-use CultuurNet\UDB3\Search\JsonDocument\Testing\AssertJsonDocumentTrait;
+use CultuurNet\UDB3\Search\Json\AssertJsonDocumentTrait;
 use PHPUnit\Framework\TestCase;
 
 class OrganizerJsonDocumentTransformerTest extends TestCase
