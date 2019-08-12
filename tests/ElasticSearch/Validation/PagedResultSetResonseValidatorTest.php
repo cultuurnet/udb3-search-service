dfa2d747d2ed536b977bf89a44941b889325a8e7
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-12 14:56:44 +0200

    PHPUnit expects an assertion or explicitly no assertion at all

--- tests/ElasticSearch/Validation/PagedResultSetResonseValidatorTest.php
+++ tests/ElasticSearch/Validation/PagedResultSetResonseValidatorTest.php
@@ -36,6 +36,7 @@ class PagedResultSetResponseValidatorTest extends TestCase
         ];
 
         $this->validator->validate($response);
+        $this->expectNotToPerformAssertions();
     }
 
     /**
