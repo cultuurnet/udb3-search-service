ec292e0b31a4b333a200372078a6aca0fc9d38d0
Bert Ramakers <bert@madewithlove.be>
2019-08-21 11:52:24 +0200

    III-3041 Use standard lowercase analyzer instead of language-specific analyzers

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20190618070000;
+    const UDB3_CORE = 20190821114700;
     const GEOSHAPES = 20190111135400;
 }
