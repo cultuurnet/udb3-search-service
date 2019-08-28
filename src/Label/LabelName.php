30278c7e53e1ca2b1621343ca962de1d688ac6e8
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-28 11:58:53 +0200

    Add internal and cleaned up VO for LabelName

---
+++ src/Label/LabelName.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Label;
+
+use ValueObjects\StringLiteral\StringLiteral;
+
+final class LabelName extends StringLiteral
+{
+    public function __construct(string $value)
+    {
+        $value = trim($value);
+
+        parent::__construct($value);
+
+        if (false !== strpos($value, ';')) {
+            throw new \InvalidArgumentException(
+                "Value for argument $value should not contain semicolons."
+            );
+        }
+
+        $length = mb_strlen($value);
+        if ($length < 2) {
+            throw new \InvalidArgumentException(
+                "Value for argument $value should not be shorter than 2 chars."
+            );
+        }
+
+        if ($length > 255) {
+            throw new \InvalidArgumentException(
+                "Value for argument $value should not be longer than 255 chars."
+            );
+        }
+    }
+}
