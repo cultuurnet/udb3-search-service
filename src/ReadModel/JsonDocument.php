972b00d1fd1fcde3d0757df730113a622ede7516
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-28 14:02:35 +0200

    Internalize a JsonDocument VO

---
+++ src/ReadModel/JsonDocument.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\ReadModel;
+
+use Broadway\ReadModel\ReadModelInterface;
+
+final class JsonDocument implements ReadModelInterface
+{
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $body;
+
+    public function __construct(string $id, string $rawBody = '{}')
+    {
+        $this->id = $id;
+        $this->body = $rawBody;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getBody(): stdClass
+    {
+        return json_decode($this->body);
+    }
+
+    public function getRawBody(): string
+    {
+        return $this->body;
+    }
+
+    public function withBody(stdClass $body): JsonDocument
+    {
+        return new self($this->id, json_encode($body));
+    }
+
+    public function apply(callable $fn): JsonDocument
+    {
+        $body = $fn($this->getBody());
+        return $this->withBody($body);
+    }
+}
