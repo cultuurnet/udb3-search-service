3e3056822a050ead214a2b207bfe2445dcb36800
Jonas Drieghe <jonasd@madewithlove.be>
2019-08-28 15:09:16 +0200

    Internalize OfferType VO

---
+++ src/Offer/OfferType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CultuurNet\UDB3\Search\Offer;
+
+use ValueObjects\Enum\Enum;
+
+/**
+ * @method static OfferType EVENT()
+ * @method static OfferType PLACE()
+ */
+class OfferType extends Enum
+{
+    const EVENT = 'Event';
+    const PLACE = 'Place';
+
+    public static function fromCaseInsensitiveValue($value)
+    {
+        return self::fromNative(ucfirst(strtolower($value)));
+    }
+}
