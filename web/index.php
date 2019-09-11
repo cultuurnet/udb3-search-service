d7b434bb4490ced0739efbdb1aeedd5d4ca9b187
Jonas Drieghe <jonasd@madewithlove.be>
2019-09-11 16:01:42 +0200

    Add basic application layout

--- web/index.php
+++ web/index.php
@@ -2,9 +2,21 @@
 
 use League\Container\Container;
 use League\Container\ReflectionContainer;
+use League\Route\Router;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiStreamEmitter;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
 
 $container->delegate(new ReflectionContainer());
+
+$router = new Router();
+
+$request = ServerRequestFactory::createFromGlobals();
+
+$response = $router->dispatch($request);
+
+$emitter = new SapiStreamEmitter();
+$emitter->emit($response);
