61d12b5f2825492f4e725ac045e591714ef2259a
Jonas Drieghe <jonasd@madewithlove.be>
2019-09-11 15:29:30 +0200

    Rename old index.php to index_silex.php and start from scratch

---
+++ web/index_silex.php
@@ -0,0 +1,81 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use CultuurNet\UDB3\SearchService\ApiGuard\ApiGuardServiceProvider;
+use CultuurNet\UDB3\SearchService\Event\EventControllerProvider;
+use CultuurNet\UDB3\SearchService\Offer\OfferControllerProvider;
+use CultuurNet\UDB3\SearchService\Organizer\OrganizerControllerProvider;
+use CultuurNet\UDB3\SearchService\Place\PlaceControllerProvider;
+use Qandidate\Toggle\ToggleManager;
+use Silex\Application;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+use ValueObjects\StringLiteral\StringLiteral;
+
+/** @var Application $app */
+$app = require __DIR__ . '/../bootstrap.php';
+
+/**
+ * Allow to use services as controllers.
+ */
+$app->register(new ServiceControllerServiceProvider());
+
+/**
+ * Return exceptions as APIProblem responses.
+ * In debug mode the standard Silex error page is shown with stack trace.
+ */
+if (!$app['config']['debug']) {
+    $app->register(
+        new \CultuurNet\UDB3\SearchService\Error\HttpErrorHandlerProvider()
+    );
+}
+
+/**
+ * API key authentication.
+ */
+$app->register(new ApiGuardServiceProvider());
+
+/** @var ToggleManager $toggles */
+$toggles = $app['toggles'];
+
+if ($toggles->active('authentication', $app['toggles.context'])) {
+    $app->before(
+        function (Request $request, Application $app) {
+            if ($request->getMethod() === "OPTIONS" && $request->headers->has("Access-Control-Request-Method")) {
+                return;
+            }
+            $app['auth.request_authenticator']->authenticate($request);
+        }
+    );
+}
+
+$app->mount('organizers', new OrganizerControllerProvider());
+
+$app->mount(
+    'offers',
+    new OfferControllerProvider(
+        new StringLiteral($app['config']['elasticsearch']['region']['read_index']),
+        new StringLiteral($app['config']['elasticsearch']['region']['document_type'])
+    )
+);
+
+$app->mount(
+    'events',
+    new EventControllerProvider(
+        new StringLiteral($app['config']['elasticsearch']['region']['read_index']),
+        new StringLiteral($app['config']['elasticsearch']['region']['document_type'])
+    )
+);
+
+$app->mount(
+    'places',
+    new PlaceControllerProvider(
+        new StringLiteral($app['config']['elasticsearch']['region']['read_index']),
+        new StringLiteral($app['config']['elasticsearch']['region']['document_type'])
+    )
+);
+
+$app->after($app['cors']);
+
+$app->run();
