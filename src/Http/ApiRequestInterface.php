c1382028dd433472dc93b4c1bd3ddcf0bc680594
zvonimir <zspajich@gmail.com>
2019-09-19 15:15:00 +0200

    Add custom ApiRequestInterface

---
+++ src/Http/ApiRequestInterface.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Interface ApiRequestInterface
+ *
+ * Custom interface that adds some additional
+ * helper methods to the request.
+ *
+ * @package CultuurNet\UDB3\Search\Http
+ */
+interface ApiRequestInterface extends ServerRequestInterface
+{
+    public function getQueryParam(string $name, $default = null);
+    
+    public function hasQueryParam(string $name): bool;
+}
