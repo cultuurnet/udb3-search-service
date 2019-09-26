f1c48c92b6972431b04c23f1f5c3045668a277b3
zvonimir <zspajich@gmail.com>
2019-09-26 12:32:46 +0200

    Use separate container file in index.php

--- web/index.php
+++ web/index.php
@@ -1,41 +1,17 @@
 <?php
 
 use CultuurNet\UDB3\Search\Http\ApiRequest;
-use CultuurNet\UDB3\SearchService\LeagueElasticSearchProvider;
-use CultuurNet\UDB3\SearchService\Offer\LeagueOfferProvider;
-use CultuurNet\UDB3\SearchService\Organizer\LeagueOrganizerServiceProvider;
-use CultuurNet\UDB3\SearchService\RoutingServiceProvider;
 use League\Container\Container;
-use League\Container\ReflectionContainer;
 use League\Route\Router;
 use Slim\Psr7\Factory\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiStreamEmitter;
-use Noodlehaus\Config;
-use Noodlehaus\Parser\Yaml;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 try {
-    $container = new Container();
-    $container->delegate(new ReflectionContainer());
-    $container->add(
-        Config::class,
-        function () {
-            $configFiles = [
-                __DIR__ . '/../config.yml',
-                __DIR__ . '/../facet_mapping_facilities.yml',
-                __DIR__ . '/../facet_mapping_regions.yml',
-                __DIR__ . '/../facet_mapping_themes.yml',
-                __DIR__ . '/../facet_mapping_types.yml',
-                __DIR__ . '/../features.yml',
-            ];
-            return Config::load($configFiles, new Yaml());
-        }
-    );
     
-    $container->addServiceProvider(RoutingServiceProvider::class);
-    $container->addServiceProvider(LeagueOrganizerServiceProvider::class);
-    $container->addServiceProvider(LeagueOfferProvider::class);
-    $container->addServiceProvider(LeagueElasticSearchProvider::class);
+    /** @var Container $container */
+    $container = require __DIR__ . '/../container.php';
     
     $response = $container->get(Router::class)->dispatch(
         new ApiRequest(
