b5c719c88e9d717e05189a559287f665562d054d
zvonimir <zspajich@gmail.com>
2019-10-07 18:53:04 +0200

    Move Offer Search factory to src

--- src/Offer/OfferSearchServiceFactory.php
+++ src/Offer/OfferSearchServiceFactory.php
@@ -1,13 +1,12 @@
 <?php declare(strict_types=1);
 
-namespace CultuurNet\UDB3\SearchService;
+namespace CultuurNet\UDB3\Search\Offer;
 
 use CultuurNet\UDB3\Search\ElasticSearch\Aggregation\AggregationTransformerInterface;
 use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchPagedResultSetFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\JsonDocumentTransformingPagedResultSetFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\Offer\ElasticSearchOfferSearchService;
 use CultuurNet\UDB3\Search\JsonDocument\PassThroughJsonDocumentTransformer;
-use CultuurNet\UDB3\Search\Offer\OfferSearchServiceInterface;
 use Elasticsearch\Client;
 use ValueObjects\StringLiteral\StringLiteral;
 
@@ -29,7 +28,7 @@ class OfferSearchServiceFactory
         $this->aggregationTransformer = $aggregationTransformer;
     }
     
-    public function createFor(string $readIndex, string $documentType) : OfferSearchServiceInterface
+    public function createFor(string $readIndex, string $documentType): OfferSearchServiceInterface
     {
         return new ElasticSearchOfferSearchService(
             $this->client,
