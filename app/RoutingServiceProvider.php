4978a190d937667736394ecadc63f8ed5a3a8e81
Bert Ramakers <bert@madewithlove.be>
2019-10-15 15:38:35 +0200

    Configure cors middleware

--- app/RoutingServiceProvider.php
+++ app/RoutingServiceProvider.php
@@ -4,8 +4,11 @@ namespace CultuurNet\UDB3\SearchService;
 
 use CultuurNet\UDB3\Search\Http\OrganizerSearchController;
 use CultuurNet\UDB3\SearchService\Http\AuthenticateRequest;
+use Fig\Http\Message\StatusCodeInterface;
 use League\Route\Router;
 use League\Route\Strategy\ApplicationStrategy;
+use Slim\Psr7\Response;
+use Tuupola\Middleware\CorsMiddleware;
 
 class RoutingServiceProvider extends BaseServiceProvider
 {
@@ -27,6 +30,24 @@ class RoutingServiceProvider extends BaseServiceProvider
                         $this->getLeagueContainer()->get(AuthenticateRequest::class)
                     );
                 }
+
+                $router->middleware(
+                    new CorsMiddleware(
+                        [
+                            "origin" => ["*"],
+                            "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+                            "headers.allow" => ["Authorization", "X-Api-Key"],
+                            "headers.expose" => [],
+                            "credentials" => true,
+                            "cache" => 0,
+                        ]
+                    )
+                );
+
+                // Register the OPTIONS method for every route to make the CORS middleware registered above work.
+                $router->options('/{path:.*}', static function () {
+                    return new Response(StatusCodeInterface::STATUS_NO_CONTENT);
+                });
 
                 $router->get('/organizers/', OrganizerSearchController::class);
                 $router->get('/offers/', ['offer_controller', '__invoke']);
