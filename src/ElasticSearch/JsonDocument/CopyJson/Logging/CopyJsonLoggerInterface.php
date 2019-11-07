62e58c30e77414dfa5eb7ef23a6dd40e82c8bfc6
Bert Ramakers <bert@madewithlove.be>
2019-11-07 14:47:40 +0100

    III-2877 Move availability copy logic to dedicated class

--- src/ElasticSearch/JsonDocument/CopyJson/Logging/CopyJsonLoggerInterface.php
+++ src/ElasticSearch/JsonDocument/CopyJson/Logging/CopyJsonLoggerInterface.php
@@ -7,4 +7,6 @@ interface CopyJsonLoggerInterface
     public function logMissingExpectedField(string $fieldName): void;
 
     public function logError(string $message): void;
+
+    public function logWarning(string $message): void;
 }
