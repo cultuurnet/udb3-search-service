56eedb94371038b4675fc0aa7bb862b0a51a2518
Bert Ramakers <bertramakers@me.com>
2020-09-28 15:57:18 +0200

    Add json transformer to embed regions in result

---
+++ tests/ElasticSearch/JsonDocument/RegionEmbeddingJsonTransformerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\ElasticSearch\JsonDocument;
+
+use PHPUnit\Framework\TestCase;
+
+final class RegionEmbeddingJsonTransformerTest extends TestCase
+{
+    /**
+     * @var RegionEmbeddingJsonTransformer
+     */
+    private $transformer;
+
+    public function setUp(): void
+    {
+        $this->transformer = new RegionEmbeddingJsonTransformer();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_embed_the_regions_if_the_original_has_one_or_more(): void
+    {
+        $original = [
+            'foo' => 'bar',
+            'regions' => [
+                'nis-20001',
+                'nis-24062C',
+                'reg-leuven',
+                'nis-24062',
+            ],
+        ];
+
+        $draft = ['other' => true];
+
+        $expected = [
+            'other' => true,
+            'regions' => [
+                'nis-20001',
+                'nis-24062C',
+                'reg-leuven',
+                'nis-24062',
+            ],
+        ];
+
+        $actual = $this->transformer->transform($original, $draft);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_embed_an_empty_array_if_no_regions_on_original(): void
+    {
+        $original = ['foo' => 'bar'];
+        $draft = ['other' => true];
+
+        $expected = [
+            'other' => true,
+            'regions' => [],
+        ];
+
+        $actual = $this->transformer->transform($original, $draft);
+        $this->assertEquals($expected, $actual);
+    }
+}
