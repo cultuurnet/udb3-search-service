bdd2315326cb1bb2358b687d9855bcba6277268b
Bert Ramakers <bertramakers@me.com>
2020-09-28 15:00:01 +0200

    Add single JsonDocumentTransformer that uses JsonTransformer

---
+++ tests/JsonDocument/JsonDocumentTransformerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\JsonDocument;
+
+use CultuurNet\UDB3\Search\JsonDocument\JsonTransformer\JsonTransformer;
+use CultuurNet\UDB3\Search\ReadModel\JsonDocument;
+use PHPUnit\Framework\TestCase;
+
+final class JsonDocumentTransformerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_return_a_new_json_document_with_the_same_id_and_transformed_json(): void
+    {
+        $id = 'd1158d19-5c12-4242-85b8-d5ec62d71ca9';
+
+        $originalData = ['foo' => 'bar', 'privateProperty' => 'secretValue'];
+        $original = new JsonDocument($id, json_encode($originalData));
+
+        $expectedData = ['foo' => 'bar'];
+        $expected = new JsonDocument($id, json_encode($expectedData));
+
+        $jsonTransformer = $this->createMock(JsonTransformer::class);
+
+        $jsonTransformer->expects($this->once())
+            ->method('transform')
+            ->with($originalData)
+            ->willReturn($expectedData);
+
+        $jsonDocumentTransformer = new JsonDocumentTransformer($jsonTransformer);
+
+        $actual = $jsonDocumentTransformer->transform($original);
+
+        $this->assertEquals($expected, $actual);
+    }
+}
