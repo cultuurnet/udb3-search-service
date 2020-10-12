e970c731fb8af734902f36a51a713b6d4c841b91
Luc Wollants <lucwollants@gmail.com>
2020-10-12 17:46:09 +0200

    III-3528 Extend SentryExceptionHandler with ApiKey

--- app/Error/SentryExceptionHandler.php
+++ app/Error/SentryExceptionHandler.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\SearchService\Error;
 
+use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
 use Sentry\State\HubInterface;
+use Sentry\State\Scope;
 use Whoops\Handler\Handler;
 
 class SentryExceptionHandler extends Handler
@@ -12,14 +14,31 @@ class SentryExceptionHandler extends Handler
     /** @var HubInterface */
     private $sentryHub;
 
-    public function __construct(HubInterface $sentryHub)
+    /**
+     * @var ApiKey|null
+     */
+    private $apiKey;
+
+    public function __construct(HubInterface $sentryHub, ?ApiKey $apiKey)
     {
         $this->sentryHub = $sentryHub;
+        $this->apiKey = $apiKey;
     }
 
     public function handle(): void
     {
+        $this->sentryHub->configureScope(function (Scope $scope) {
+            $scope->setTags($this->createTags($this->apiKey));
+        });
+
         $exception = $this->getInspector()->getException();
         $this->sentryHub->captureException($exception);
+    }
+
+    private function createTags(?ApiKey $apiKey): array
+    {
+        return [
+            'api_key' => $apiKey ? $apiKey->toNative() : 'null',
+        ];
     }
 }
