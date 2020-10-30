56def95c340195d36706e9aba089566f7290b08f
Luc Wollants <lucwollants@gmail.com>
2020-10-30 13:17:42 +0100

    III-3545 Test transforming place json with metadata

--- tests/ElasticSearch/JsonDocument/PlaceTransformerTest.php
+++ tests/ElasticSearch/JsonDocument/PlaceTransformerTest.php
@@ -279,6 +279,17 @@ class PlaceTransformerTest extends TestCase
         );
     }
 
+    /**
+     * @test
+     */
+    public function it_transforms_metadata(): void
+    {
+        $this->transformAndAssert(
+            __DIR__ . '/data/place/original-with-metadata.json',
+            __DIR__ . '/data/place/indexed-with-metadata.json'
+        );
+    }
+
     private function transformAndAssert(string $givenFilePath, string $expectedFilePath, array $expectedLogs = []): void
     {
         $original = json_decode(file_get_contents($givenFilePath), true);
