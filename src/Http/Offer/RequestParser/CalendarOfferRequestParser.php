2e6fa2ebb56158e85550dc6735ecb826d405bd42
Bert Ramakers <bertramakers@me.com>
2021-01-04 18:10:57 +0100

    Add support for status url parameter

--- src/Http/Offer/RequestParser/CalendarOfferRequestParser.php
+++ src/Http/Offer/RequestParser/CalendarOfferRequestParser.php
@@ -6,6 +6,7 @@ use CultuurNet\UDB3\Search\Http\ApiRequestInterface;
 use CultuurNet\UDB3\Search\Http\Parameters\ParameterBagInterface;
 use CultuurNet\UDB3\Search\Offer\CalendarType;
 use CultuurNet\UDB3\Search\Offer\OfferQueryBuilderInterface;
+use CultuurNet\UDB3\Search\Offer\Status;
 
 class CalendarOfferRequestParser implements OfferRequestParserInterface
 {
@@ -17,10 +18,25 @@ class CalendarOfferRequestParser implements OfferRequestParserInterface
 
         $offerQueryBuilder = $this->parseCalendarType($parameterBagReader, $offerQueryBuilder);
 
+        $statuses = $parameterBagReader->getExplodedStringFromParameter(
+            'status',
+            null,
+            function (string $status) {
+                return Status::fromNative($status);
+            }
+        );
         $dateFrom = $parameterBagReader->getDateTimeFromParameter('dateFrom');
         $dateTo = $parameterBagReader->getDateTimeFromParameter('dateTo');
-        if ($dateFrom || $dateTo) {
+
+        $hasStatuses = !empty($statuses);
+        $hasDates = !is_null($dateFrom) || !is_null($dateTo);
+
+        if ($hasStatuses && $hasDates) {
+            $offerQueryBuilder = $offerQueryBuilder->withStatusAwareDateRangeFilter($dateFrom, $dateTo, ...$statuses);
+        } elseif ($hasDates) {
             $offerQueryBuilder = $offerQueryBuilder->withDateRangeFilter($dateFrom, $dateTo);
+        } elseif ($hasStatuses) {
+            $offerQueryBuilder = $offerQueryBuilder->withStatusFilter(...$statuses);
         }
 
         return $offerQueryBuilder;
