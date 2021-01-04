e69ca334510fb131796ab51c4ce37a0173cff2de
Bert Ramakers <bertramakers@me.com>
2021-01-04 17:04:01 +0100

    Add mapping for top-level status

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20201217134800;
+    const UDB3_CORE = 20210104170300;
     const GEOSHAPES = 20190111135400;
 }
