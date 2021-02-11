879287517d770bc85253224ed34a560a9d5add3c
Bert Ramakers <bertramakers@me.com>
2021-02-11 14:18:26 +0100

    Update SchemaVersions

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -4,6 +4,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 class SchemaVersions
 {
-    const UDB3_CORE = 20210208122300;
+    const UDB3_CORE = 20210211141800;
     const GEOSHAPES = 20190111135400;
 }
