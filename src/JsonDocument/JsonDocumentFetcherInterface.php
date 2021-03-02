d435996ca7318208f6b43c94b043a93faecbc86c
Luc Wollants <lucwollants@gmail.com>
2021-03-02 15:34:07 +0100

    Add JsonDocumentFetcherInterface

---
+++ src/JsonDocument/JsonDocumentFetcherInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\JsonDocument;
+
+use CultuurNet\UDB3\Search\ReadModel\JsonDocument;
+
+interface JsonDocumentFetcherInterface
+{
+    public function withIncludeMetadata(): JsonDocumentFetcher;
+
+    public function fetch(string $documentId, string $documentIri): ?JsonDocument;
+}
