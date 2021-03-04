78791faf06ac9b8949eabc2e2ac4839ed454e4ba
Jonas Drieghe <jonasd@madewithlove.be>
2021-03-04 14:07:57 +0100

    Add CalendarSummaryFormat value object to hold type and format from url parameter

---
+++ src/Offer/CalendarSummaryFormat.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Offer;
+
+use InvalidArgumentException;
+
+final class CalendarSummaryFormat
+{
+    private const ALLOWED_TYPES = ['text', 'html'];
+    private const ALLOWED_FORMATS = ['xs', 'sm', 'md', 'lg'];
+
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $format;
+
+    public function __construct(string $type, string $format)
+    {
+        if (!in_array($type, self::ALLOWED_TYPES)) {
+            throw new InvalidArgumentException(
+                'Invalid type: ' . $type . '. Use one of: ' . join(',', self::ALLOWED_TYPES)
+            );
+        }
+
+        if (!in_array($format, self::ALLOWED_FORMATS)) {
+            throw new InvalidArgumentException(
+                'Invalid format: ' . $format . '. Use one of: ' . join(',', self::ALLOWED_FORMATS)
+            );
+        }
+
+        $this->type = $type;
+        $this->format = $format;
+    }
+
+    public static function fromCombinedParameter(string $parameter): self
+    {
+        [$format, $type] = explode('-', $parameter);
+        return new self($type, $format);
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getFormat(): string
+    {
+        return $this->format;
+    }
+}
