1c1f47b3165b341c1c5d2b74c1e7aa7add96bc7f
Luc Wollants <lucwollants@gmail.com>
2021-03-10 15:23:26 +0100

    Refactor FacetName to custom enum implementation

--- src/Offer/FacetName.php
+++ src/Offer/FacetName.php
@@ -4,20 +4,73 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Search\Offer;
 
-use ValueObjects\Enum\Enum;
+use InvalidArgumentException;
 
-/**
- * @method static FacetName REGIONS()
- * @method static FacetName TYPES()
- * @method static FacetName THEMES()
- * @method static FacetName FACILITIES()
- * @method static FacetName LABELS()
- */
-final class FacetName extends Enum
+final class FacetName
 {
     public const REGIONS = 'regions';
     public const TYPES = 'types';
     public const THEMES = 'themes';
     public const FACILITIES = 'facilities';
     public const LABELS = 'labels';
+
+    /**
+     * @var string
+     */
+    private $facetName;
+
+    public function __construct(string $facetName)
+    {
+        if (!in_array($facetName, $this->getAllowedValues())) {
+            throw new InvalidArgumentException('The given facet name ' . $facetName . ' is not supported');
+        }
+
+        $this->facetName = $facetName;
+    }
+
+    public static function regions(): self
+    {
+        return new self(self::REGIONS);
+    }
+
+    public static function types(): self
+    {
+        return new self(self::TYPES);
+    }
+
+    public static function themes(): self
+    {
+        return new self(self::THEMES);
+    }
+
+    public static function facilities(): self
+    {
+        return new self(self::FACILITIES);
+    }
+
+    public static function labels(): self
+    {
+        return new self(self::LABELS);
+    }
+
+    public function toString(): string
+    {
+        return $this->facetName;
+    }
+
+    public function sameValueAs(FacetName $otherFacetName): bool
+    {
+        return $this->toString() === $otherFacetName->toString();
+    }
+
+    private function getAllowedValues(): array
+    {
+        return [
+            self::REGIONS,
+            self::TYPES,
+            self::THEMES,
+            self::FACILITIES,
+            self::LABELS,
+        ];
+    }
 }
