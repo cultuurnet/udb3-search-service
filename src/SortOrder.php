568c79dcc846b4f5da54ee6a96c866e61d948133
Luc Wollants <lucwollants@gmail.com>
2021-03-10 14:18:20 +0100

    Refactor SortOrder to custom enum

--- src/SortOrder.php
+++ src/SortOrder.php
@@ -4,14 +4,47 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Search;
 
-use ValueObjects\Enum\Enum;
+use InvalidArgumentException;
 
-/**
- * @method static SortOrder ASC()
- * @method static SortOrder DESC()
- */
-final class SortOrder extends Enum
+final class SortOrder
 {
     public const ASC = 'asc';
     public const DESC = 'desc';
+
+    /**
+     * @var string
+     */
+    private $order;
+
+    public function __construct(string $order)
+    {
+        if (!\in_array($order, $this->getAllowedValues())) {
+            throw new InvalidArgumentException('The given sort order ' . $order . ' is not asc or desc');
+        }
+
+        $this->order = $order;
+    }
+
+    public static function asc(): self
+    {
+        return new self(self::ASC);
+    }
+
+    public static function desc(): self
+    {
+        return new self(self::DESC);
+    }
+
+    public function toString(): string
+    {
+        return $this->order;
+    }
+
+    private function getAllowedValues(): array
+    {
+        return [
+            self::ASC,
+            self::DESC,
+        ];
+    }
 }
