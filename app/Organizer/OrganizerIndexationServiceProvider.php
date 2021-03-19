eb8605c1885bca75b25f8b5b4321b6129f3c781d
Bert Ramakers <bertramakers@me.com>
2021-03-19 13:49:22 +0100

    Inline creation of organizer services that are only injected once

--- app/Organizer/OrganizerIndexationServiceProvider.php
+++ app/Organizer/OrganizerIndexationServiceProvider.php
@@ -27,23 +27,7 @@ final class OrganizerIndexationServiceProvider extends BaseServiceProvider
         $this->add(
             'organizer_search_projector',
             function () {
-                $service = new TransformingJsonDocumentIndexService(
-                    $this->get(JsonDocumentFetcher::class),
-                    $this->get('organizer_elasticsearch_transformer'),
-                    $this->get('organizer_elasticsearch_repository')
-                );
-
-                $service->setLogger($this->get('logger.amqp.udb3_consumer'));
-
-                return new OrganizerSearchProjector($service);
-            },
-            'event_bus_subscribers'
-        );
-
-        $this->add(
-            'organizer_elasticsearch_transformer',
-            function () {
-                return new JsonDocumentTransformer(
+                $transformer = new JsonDocumentTransformer(
                     new OrganizerTransformer(
                         new JsonTransformerPsrLogger(
                             $this->get('logger.amqp.udb3_consumer')
@@ -51,19 +35,25 @@ final class OrganizerIndexationServiceProvider extends BaseServiceProvider
                         new PathEndIdUrlParser()
                     )
                 );
-            }
-        );
 
-        $this->add(
-            'organizer_elasticsearch_repository',
-            function () {
-                return new ElasticSearchDocumentRepository(
+                $repository = new ElasticSearchDocumentRepository(
                     $this->get(Client::class),
                     $this->parameter('elasticsearch.organizer.write_index'),
                     $this->parameter('elasticsearch.organizer.document_type'),
                     $this->get('elasticsearch_indexation_strategy')
                 );
-            }
+
+                $service = new TransformingJsonDocumentIndexService(
+                    $this->get(JsonDocumentFetcher::class),
+                    $transformer,
+                    $repository
+                );
+
+                $service->setLogger($this->get('logger.amqp.udb3_consumer'));
+
+                return new OrganizerSearchProjector($service);
+            },
+            'event_bus_subscribers'
         );
     }
 }
