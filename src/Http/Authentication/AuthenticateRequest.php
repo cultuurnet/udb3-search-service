3a6a42ddd05a15fadff3f1ee38e1934546d96bc2
Luc Wollants <lucwollants@gmail.com>
2021-06-14 16:37:16 +0200

    Extend Authentication middleware with support for clientId

--- src/Http/Authentication/AuthenticateRequest.php
+++ src/Http/Authentication/AuthenticateRequest.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Search\Http\Authentication;
 
+use Auth0\SDK\API\Management as Auth0Management;
 use CultureFeed_Consumer;
 use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\InvalidApiKey;
+use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\InvalidClientId;
 use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\MissingCredentials;
 use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\BlockedApiKey;
+use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\MissingSapiScope;
 use CultuurNet\UDB3\Search\Http\Authentication\ApiProblems\RemovedApiKey;
 use Exception;
 use ICultureFeed;
@@ -29,10 +32,16 @@ final class AuthenticateRequest implements MiddlewareInterface
      */
     private $cultureFeed;
 
-    public function __construct(Container $container, ICultureFeed $cultureFeed)
+    /**
+     * @var Auth0Management
+     */
+    private $auth0Management;
+
+    public function __construct(Container $container, ICultureFeed $cultureFeed, Auth0Management $auth0Management)
     {
         $this->container = $container;
         $this->cultureFeed = $cultureFeed;
+        $this->auth0Management = $auth0Management;
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -41,12 +50,42 @@ final class AuthenticateRequest implements MiddlewareInterface
             return $handler->handle($request);
         }
 
+        $clientId = $this->getClientId($request);
+
+        if ($clientId !== null) {
+            return $this->handleClientId($request, $handler, $clientId);
+        }
+
         $apiKey = $this->getApiKey($request);
 
         if (empty($apiKey)) {
             return (new MissingCredentials())->toResponse();
         }
 
+        return $this->handleApiKey($request, $handler, $apiKey);
+    }
+
+    private function handleClientId(ServerRequestInterface $request, RequestHandlerInterface $handler, string $clientId): ResponseInterface
+    {
+        try {
+            $client = $this->auth0Management->clients()->get($clientId, ['client_id', 'client_metadata'], true);
+        } catch (Exception $exception) {
+            return (new InvalidClientId($clientId))->toResponse();
+        }
+
+        if (empty($client['client_metadata']['sapi3'])) {
+            return (new MissingSapiScope($clientId))->toResponse();
+        }
+
+        $this->container
+            ->extend(Consumer::class)
+            ->setConcrete(new Consumer($clientId, null));
+
+        return $handler->handle($request);
+    }
+
+    private function handleApiKey(ServerRequestInterface $request, RequestHandlerInterface $handler, string $apiKey): ResponseInterface
+    {
         try {
             /** @var CultureFeed_Consumer $cultureFeedConsumer */
             $cultureFeedConsumer = $this->cultureFeed->getServiceConsumerByApiKey($apiKey, true);
@@ -82,6 +121,19 @@ final class AuthenticateRequest implements MiddlewareInterface
 
         if ($this->getParamValue($request, 'apiKey')) {
             return $this->getParamValue($request, 'apiKey');
+        }
+
+        return null;
+    }
+
+    private function getClientId(ServerRequestInterface $request): ?string
+    {
+        if ($this->getHeaderValue($request, 'x-client-id')) {
+            return $this->getHeaderValue($request, 'x-client-id');
+        }
+
+        if ($this->getParamValue($request, 'clientId')) {
+            return $this->getParamValue($request, 'clientId');
         }
 
         return null;
