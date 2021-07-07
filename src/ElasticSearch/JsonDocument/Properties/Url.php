77f6db715f108db3a400349a5bc4e5a43151daba
Bert Ramakers <bertramakers@me.com>
2021-07-07 12:38:30 +0200

    Use parse_url() parts instead of regex to create normalized URL

--- src/ElasticSearch/JsonDocument/Properties/Url.php
+++ src/ElasticSearch/JsonDocument/Properties/Url.php
@@ -8,10 +8,6 @@ use InvalidArgumentException;
 
 final class Url
 {
-    // Important! The ? after .* is required to make the match non-greedy so it doesn't capture the trailing slash
-    // before it gets to that check.
-    private const NORMALIZATION_REGEX = '/^(https?:\/\/)?(www.)?(.*?)(\/?)$/i';
-
     /**
      * @var string
      */
@@ -26,10 +22,7 @@ final class Url
     {
         $this->urlParts = parse_url($url);
 
-        // Normally any string should match the normalization regex since everything is optional except for the middle
-        // part which allows any character(s) anyway. But just in case check it so we don't run into an error down the
-        // line when getNormalizedUrl() gets called.
-        if (!is_array($this->urlParts) || !isset($this->urlParts['host']) || !preg_match(self::NORMALIZATION_REGEX, $url)) {
+        if (!is_array($this->urlParts) || !isset($this->urlParts['host'])) {
             throw new InvalidArgumentException('Url ' . $url . ' is not supported');
         }
 
@@ -44,15 +37,16 @@ final class Url
      */
     public function getNormalizedUrl(): string
     {
-        // $matches will be filled with 5 values:
-        // 0. The full match (i.e. the input string if the regex matches)
-        // 1. http:// or https:// or an empty string
-        // 2. www. or an empty string
-        // 3. The normalized URL (i.e. everything in the middle)
-        // 4. Trailing slash or empty string
-        $matches = [];
-        preg_match(self::NORMALIZATION_REGEX, $this->url, $matches);
-        return $matches[3];
+        return implode(
+            '',
+            [
+                $this->getDomain(),
+                isset($this->urlParts['port']) ? ':' . $this->urlParts['port'] : null,
+                isset($this->urlParts['path']) ? rtrim($this->urlParts['path'], '/') : null,
+                isset($this->urlParts['query']) ? '?' . $this->urlParts['query'] : null,
+                isset($this->urlParts['fragment']) ? '#' . $this->urlParts['fragment'] : null,
+            ]
+        );
     }
 
     public function getDomain(): string
