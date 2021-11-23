15acc89ca51b8ea67afdbe75344e51ced44f13fe
Bert Ramakers <bertramakers@me.com>
2021-11-23 18:12:26 +0100

    Update schema version to make re-index easier

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20211110101300;
+    public const UDB3_CORE = 20211123172800;
     public const GEOSHAPES = 20190111135400;
 }
