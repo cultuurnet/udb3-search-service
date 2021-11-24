4ccba41d519b7739522be3a63e7d6fb3447d3658
Bert Ramakers <bertramakers@me.com>
2021-11-24 13:48:59 +0100

    Update recommendationFor mapping to nested object

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20211123172800;
+    public const UDB3_CORE = 20211124134500;
     public const GEOSHAPES = 20190111135400;
 }
