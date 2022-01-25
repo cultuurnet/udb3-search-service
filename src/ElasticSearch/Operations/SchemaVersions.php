34ae931a32b5f6d0a3ca2d2d890098caf62021fa
Luc Wollants <lucwollants@gmail.com>
2022-01-25 14:21:27 +0100

    Increment UDB3_CORE

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20211124134500;
+    public const UDB3_CORE = 20220125140200;
     public const GEOSHAPES = 20190111135400;
 }
