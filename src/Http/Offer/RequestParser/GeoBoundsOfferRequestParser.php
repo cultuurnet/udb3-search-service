2a045b98a911de029701cdcf7d9e1715914b215c
Luc Wollants <lucwollants@gmail.com>
2022-01-26 10:16:52 +0100

    Use GeoBoundsParametersFactory inside GeoBoundsOfferRequestParser

--- src/Http/Offer/RequestParser/GeoBoundsOfferRequestParser.php
+++ src/Http/Offer/RequestParser/GeoBoundsOfferRequestParser.php
@@ -4,53 +4,29 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Search\Http\Offer\RequestParser;
 
-use CultuurNet\UDB3\Search\Geocoding\Coordinate\Coordinates;
-use CultuurNet\UDB3\Search\Geocoding\Coordinate\Latitude;
-use CultuurNet\UDB3\Search\Geocoding\Coordinate\Longitude;
-use CultuurNet\UDB3\Search\GeoBoundsParameters;
 use CultuurNet\UDB3\Search\Http\ApiRequestInterface;
+use CultuurNet\UDB3\Search\Http\Parameters\GeoBoundsParametersFactory;
 use CultuurNet\UDB3\Search\Offer\OfferQueryBuilderInterface;
-use CultuurNet\UDB3\Search\UnsupportedParameterValue;
 
 final class GeoBoundsOfferRequestParser implements OfferRequestParserInterface
 {
-    private const BOUNDS_REGEX = '([0-9\.-]+),([0-9\.-]+)\|([0-9\.-]+),([0-9\.-]+)';
+    private GeoBoundsParametersFactory $geoBoundsParametersFactory;
+
+    public function __construct(GeoBoundsParametersFactory $geoBoundsParametersFactory)
+    {
+        $this->geoBoundsParametersFactory = $geoBoundsParametersFactory;
+    }
 
     public function parse(
         ApiRequestInterface $request,
         OfferQueryBuilderInterface $offerQueryBuilder
     ): OfferQueryBuilderInterface {
-        $bounds = $request->getQueryParam('bounds', false);
-        if (!$bounds) {
+        $geoBoundsParameters = $this->geoBoundsParametersFactory->fromApiRequest($request);
+
+        if ($geoBoundsParameters === null) {
             return $offerQueryBuilder;
         }
 
-        $matches = [];
-        if (!preg_match('/' . self::BOUNDS_REGEX . '/', $bounds, $matches)) {
-            throw new UnsupportedParameterValue(
-                'Bounds parameter should be in the "southWestLat,southWestLong|northEastLat,NorthEastLong" format.'
-            );
-        }
-
-        $southWestLat = (float) $matches[1];
-        $southWestLong = (float) $matches[2];
-        $northEastLat = (float) $matches[3];
-        $northEastLong = (float) $matches[4];
-
-        $southWest = new Coordinates(
-            new Latitude($southWestLat),
-            new Longitude($southWestLong)
-        );
-
-        $northEast = new Coordinates(
-            new Latitude($northEastLat),
-            new Longitude($northEastLong)
-        );
-
-        $offerQueryBuilder = $offerQueryBuilder->withGeoBoundsFilter(
-            new GeoBoundsParameters($northEast, $southWest)
-        );
-
-        return $offerQueryBuilder;
+        return $offerQueryBuilder->withGeoBoundsFilter($geoBoundsParameters);
     }
 }
