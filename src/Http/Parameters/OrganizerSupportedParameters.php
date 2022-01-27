85e507d56d27a278ceb9dfa31dbfedd88afc3541
Luc Wollants <lucwollants@gmail.com>
2022-01-27 11:07:34 +0100

    Add `facets` as supported organizer parameter

--- src/Http/Parameters/OrganizerSupportedParameters.php
+++ src/Http/Parameters/OrganizerSupportedParameters.php
@@ -19,6 +19,7 @@ final class OrganizerSupportedParameters extends AbstractSupportedParameters
             'coordinates',
             'distance',
             'bounds',
+            'facets',
             'creator',
             'labels',
             'textLanguages',
