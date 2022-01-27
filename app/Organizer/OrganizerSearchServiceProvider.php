f70821b5c56ff772cd049a013617d597a0d1a5c5
Luc Wollants <lucwollants@gmail.com>
2022-01-27 11:20:35 +0100

    Bootstrap `OrganizerSearchController` with Facet normalizer

--- app/Organizer/OrganizerSearchServiceProvider.php
+++ app/Organizer/OrganizerSearchServiceProvider.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\SearchService\Organizer;
 
-use CultuurNet\UDB3\Search\ElasticSearch\Aggregation\NullAggregationTransformer;
+use CultuurNet\UDB3\Search\ElasticSearch\Aggregation\NodeMapAggregationTransformer;
 use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchDistanceFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\ElasticSearchPagedResultSetFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\LuceneQueryStringFactory;
 use CultuurNet\UDB3\Search\ElasticSearch\Organizer\ElasticSearchOrganizerQueryBuilder;
 use CultuurNet\UDB3\Search\ElasticSearch\Organizer\ElasticSearchOrganizerSearchService;
 use CultuurNet\UDB3\Search\Http\Authentication\Consumer;
+use CultuurNet\UDB3\Search\Http\NodeAwareFacetTreeNormalizer;
 use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\CompositeOrganizerRequestParser;
 use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\DistanceOrganizerRequestParser;
 use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\GeoBoundsOrganizerRequestParser;
@@ -19,6 +20,7 @@ use CultuurNet\UDB3\Search\Http\Organizer\RequestParser\WorkflowStatusOrganizerR
 use CultuurNet\UDB3\Search\Http\OrganizerSearchController;
 use CultuurNet\UDB3\Search\Http\Parameters\GeoBoundsParametersFactory;
 use CultuurNet\UDB3\Search\Http\Parameters\GeoDistanceParametersFactory;
+use CultuurNet\UDB3\Search\Offer\FacetName;
 use CultuurNet\UDB3\SearchService\BaseServiceProvider;
 use Elasticsearch\Client;
 
@@ -46,19 +48,25 @@ final class OrganizerSearchServiceProvider extends BaseServiceProvider
                     ->withParser(new SortByOrganizerRequestParser());
 
                 return new OrganizerSearchController(
-                    new ElasticSearchOrganizerQueryBuilder(),
+                    new ElasticSearchOrganizerQueryBuilder(
+                        $this->parameter('elasticsearch.aggregation_size')
+                    ),
                     new ElasticSearchOrganizerSearchService(
                         $this->get(Client::class),
                         $this->parameter('elasticsearch.organizer.read_index'),
                         $this->parameter('elasticsearch.organizer.document_type'),
                         new ElasticSearchPagedResultSetFactory(
-                            new NullAggregationTransformer()
+                            new NodeMapAggregationTransformer(
+                                FacetName::regions(),
+                                $this->parameter('facet_mapping_regions')
+                            )
                         )
                     ),
                     $this->parameter('elasticsearch.region.read_index'),
                     $this->parameter('elasticsearch.region.document_type'),
                     $requestParser,
                     new LuceneQueryStringFactory(),
+                    new NodeAwareFacetTreeNormalizer(),
                     $this->get(Consumer::class)
                 );
             }
