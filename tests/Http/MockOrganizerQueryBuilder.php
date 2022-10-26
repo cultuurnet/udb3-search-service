39001700dc128b9beadfe660dd1fe582b454d1ed
Jonas <jonas@cultuurnet.be>
2022-10-26 13:32:37 +0200

    Implement withStartAndLimit() in tests

--- tests/Http/MockOrganizerQueryBuilder.php
+++ tests/Http/MockOrganizerQueryBuilder.php
@@ -194,6 +194,14 @@ final class MockOrganizerQueryBuilder implements OrganizerQueryBuilderInterface
         return $c;
     }
 
+    public function withStartAndLimit(Start $start, Limit $limit): MockOrganizerQueryBuilder
+    {
+        $c = clone $this;
+        $c->mockQuery['start'] = $start->toInteger();
+        $c->mockQuery['limit'] = $limit->toInteger();
+        return $c;
+    }
+
     public function getLimit(): Limit
     {
         if (!isset($this->mockQuery['limit'])) {
