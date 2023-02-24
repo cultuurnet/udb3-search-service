f65bf86e3a5cea24ec59a30075888ef68085d093
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-02-24 13:50:32 +0100

    make requesthandlers for contributors

---
+++ src/Http/Offer/RequestParser/ContributorsRequestParser.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Http\Offer\RequestParser;
+
+use CultuurNet\UDB3\Search\Http\ApiRequestInterface;
+use CultuurNet\UDB3\Search\Offer\OfferQueryBuilderInterface;
+
+final class ContributorsRequestParser implements OfferRequestParserInterface
+{
+    public function parse(
+        ApiRequestInterface $request,
+        OfferQueryBuilderInterface $offerQueryBuilder
+    ): OfferQueryBuilderInterface {
+        $parameterBagReader = $request->getQueryParameterBag();
+        $contributor = $parameterBagReader->getStringFromParameter('contributor');
+
+        if (!is_null($contributor)) {
+            $offerQueryBuilder = $offerQueryBuilder->withContributorsFilter($contributor);
+        }
+        $offerQueryBuilder->withAgeRangeFilter();
+
+        return $offerQueryBuilder;
+    }
+}
---
+++ src/Http/Organizer/RequestParser/ContributorsRequestParser.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Http\Organizer\RequestParser;
+
+use CultuurNet\UDB3\Search\Http\ApiRequestInterface;
+use CultuurNet\UDB3\Search\Organizer\OrganizerQueryBuilderInterface;
+
+final class ContributorsRequestParser implements OrganizerRequestParser
+{
+    public function parse(
+        ApiRequestInterface $request,
+        OrganizerQueryBuilderInterface $organizerQueryBuilder
+    ): OrganizerQueryBuilderInterface {
+        $parameterBagReader = $request->getQueryParameterBag();
+        $contributor = $parameterBagReader->getStringFromParameter('contributor');
+
+        if (!is_null($contributor)) {
+            $organizerQueryBuilder = $organizerQueryBuilder->withContributorsFilter($contributor);
+        }
+
+        return $organizerQueryBuilder;
+    }
+}
