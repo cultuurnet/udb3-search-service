667f6ef00b643a39fac9ec6876f2809f5242eb3d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-01 10:52:47 +0100

    Refactor GuzzleJsonDocumentFetcher to store & refresh token

--- src/JsonDocument/GuzzleJsonDocumentFetcher.php
+++ src/JsonDocument/GuzzleJsonDocumentFetcher.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\Search\JsonDocument;
 
 use CultuurNet\UDB3\Search\Http\Authentication\Auth0Client;
+use CultuurNet\UDB3\Search\Http\Authentication\Auth0Token;
 use CultuurNet\UDB3\Search\ReadModel\JsonDocument;
 use GuzzleHttp\ClientInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 final class GuzzleJsonDocumentFetcher implements JsonDocumentFetcher
@@ -19,12 +21,15 @@ final class GuzzleJsonDocumentFetcher implements JsonDocumentFetcher
 
     private Auth0Client $auth0Client;
 
+    private ?Auth0Token $auth0Token;
+
     public function __construct(ClientInterface $httpClient, LoggerInterface $logger, Auth0Client $auth0Client)
     {
         $this->httpClient = $httpClient;
         $this->includeMetadata = false;
         $this->logger = $logger;
         $this->auth0Client = $auth0Client;
+        $this->auth0Token = $this->auth0Client->getToken();
     }
 
     public function withIncludeMetadata(): JsonDocumentFetcher
@@ -36,11 +41,15 @@ final class GuzzleJsonDocumentFetcher implements JsonDocumentFetcher
 
     public function fetch(string $documentId, string $documentIri): ?JsonDocument
     {
-        $response = $this->httpClient->request(
-            'GET',
-            $documentIri,
-            array_merge($this->getQuery($this->includeMetadata), $this->getHeader())
-        );
+        $attempt = 0;
+        $maximumAttempts = 2;
+
+        $response = $this->getResponse($documentIri);
+        while ($response->getStatusCode() === 401 && $attempt < $maximumAttempts) {
+            $attempt++;
+            $this->refreshToken();
+            $response = $this->getResponse($documentIri);
+        }
 
         if ($response->getStatusCode() !== 200) {
             $this->logger->error(
@@ -58,6 +67,15 @@ final class GuzzleJsonDocumentFetcher implements JsonDocumentFetcher
         return new JsonDocument($documentId, (string) $response->getBody());
     }
 
+    private function getResponse(string $documentIri): ResponseInterface
+    {
+        return $this->httpClient->request(
+            'GET',
+            $documentIri,
+            array_merge($this->getQuery($this->includeMetadata), $this->getHeader())
+        );
+    }
+
     private function getQuery(bool $includeMetadata): array
     {
         if (!$includeMetadata) {
@@ -72,9 +90,14 @@ final class GuzzleJsonDocumentFetcher implements JsonDocumentFetcher
         ];
     }
 
+    private function refreshToken(): void
+    {
+        $this->auth0Token = $this->auth0Client->getToken();
+    }
+
     private function getHeader(): array
     {
-        $token = $this->auth0Client->getToken();
+        $token = $this->auth0Token;
         if ($token === null) {
             return [];
         }
