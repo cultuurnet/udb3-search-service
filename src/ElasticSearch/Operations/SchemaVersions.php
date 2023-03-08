ef4751312e92dbf30a6d9a91b2723cdcddcc105d
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-08 09:51:03 +0100

    update UDB3_CORE version

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20230227083442;
+    public const UDB3_CORE = 20230308085903;
     public const GEOSHAPES = 20190111135400;
 }
