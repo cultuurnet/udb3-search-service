15eddffe212ddaaecd3c23fc9a053668c6651ed4
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-03-09 10:35:33 +0100

    test PredefinedFields for Organizer

---
+++ tests/ElasticSearch/Organizer/OrganizerPredefinedQueryStringFieldsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\ElasticSearch\Organizer;
+
+use CultuurNet\UDB3\Search\Language\Language;
+use PHPUnit\Framework\TestCase;
+
+final class OrganizerPredefinedQueryStringFieldsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_predefined_query_string_fields()
+    {
+        $fields = (new OrganizerPredefinedQueryStringFields())->getPredefinedFields(
+            new Language('nl'),
+            new Language('de')
+        );
+
+        $this->assertEquals(
+            [
+                'id',
+                'labels_free_text',
+                'name.nl',
+                'description.nl',
+                'address.nl.addressLocality',
+                'address.nl.postalCode',
+                'address.nl.streetAddress',
+                'name.de',
+                'description.de',
+                'address.de.addressLocality',
+                'address.de.postalCode',
+                'address.de.streetAddress',
+            ],
+            $fields
+        );
+    }
+}
