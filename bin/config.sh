8b1c4113de237d77a06a87c51b171b1c35e6614c
Koen Eelen <koen.eelenpubliq.be>
2023-07-27 13:19:03 +0200

    Moved copying of config files from vagrant to appconfig repo

--- bin/config.sh
+++ bin/config.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-DIR="../udb3-vagrant/config/"
+DIR="../appconfig/files/udb3/docker"
 if [ -d "$DIR" ]; then
   cp "$DIR"/udb3-search-service/config.yml .
   cp "$DIR"/udb3-search-service/facet_mapping_facilities.yml .
   cp "$DIR"/udb3-search-service/facet_mapping_themes.yml .
   cp "$DIR"/udb3-search-service/facet_mapping_types.yml .
   cp "$DIR"/udb3-search-service/features.yml .
-  cp "$DIR"/keys/public-auth0.pem .
-
-  sed -i '' 's!http://elasticsearch.dev:9200!elasticsearch!g' config.yml
-  sed -i '' 's!127.0.0.1!host.docker.internal!g' config.yml
-else
+  cp "$DIR"/udb3-backend/public-auth0.pem .
+  else
   echo "Error: missing udb3-vagrant see docker.md prerequisites to fix this."
   exit 1
 fi
 
-DIR="../geojson-data/output/"
+DIR="../geojson-data/output"
 if [ -d "$DIR" ]; then
   cp "$DIR"/facet_mapping_regions.yml .
 else
