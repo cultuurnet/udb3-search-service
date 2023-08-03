cf1401409dd38c5af499a3a206e5960c39b5a280
Koen Eelen <koen.eelenpubliq.be>
2023-08-03 15:24:03 +0200

    Copy entire config dir instead of file per file

--- bin/config.sh
+++ bin/config.sh
@@ -2,14 +2,10 @@
 
 DIR="../appconfig/files/udb3/docker"
 if [ -d "$DIR" ]; then
-  cp "$DIR"/udb3-search-service/config.yml .
-  cp "$DIR"/udb3-search-service/facet_mapping_facilities.yml .
-  cp "$DIR"/udb3-search-service/facet_mapping_themes.yml .
-  cp "$DIR"/udb3-search-service/facet_mapping_types.yml .
-  cp "$DIR"/udb3-search-service/features.yml .
+  cp -R "$DIR"/udb3-search-service/* .
   cp "$DIR"/udb3-backend/public-auth0.pem .
   else
-  echo "Error: missing udb3-vagrant see docker.md prerequisites to fix this."
+  echo "Error: missing appconfig see docker.md prerequisites to fix this."
   exit 1
 fi
 
