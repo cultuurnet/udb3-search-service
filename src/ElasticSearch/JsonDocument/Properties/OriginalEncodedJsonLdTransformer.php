23fd9ef69e3becf5906f34d400f03e97f38d4804
Koen Eelen <koen.eelenpubliq.be>
2023-11-15 16:38:49 +0100

    remove code that removes contributors

--- src/ElasticSearch/JsonDocument/Properties/OriginalEncodedJsonLdTransformer.php
+++ src/ElasticSearch/JsonDocument/Properties/OriginalEncodedJsonLdTransformer.php
@@ -9,14 +9,8 @@ use CultuurNet\UDB3\Search\JsonDocument\JsonTransformer;
 
 final class OriginalEncodedJsonLdTransformer implements JsonTransformer
 {
-    private const CONTRIBUTORS = 'contributors';
-
     public function transform(array $from, array $draft = []): array
     {
-        if (isset($from[self::CONTRIBUTORS])) {
-            unset($from[self::CONTRIBUTORS]);
-        }
-
         $draft['originalEncodedJsonLd'] = Json::encodeWithOptions((object)$from, JSON_UNESCAPED_SLASHES);
         return $draft;
     }
