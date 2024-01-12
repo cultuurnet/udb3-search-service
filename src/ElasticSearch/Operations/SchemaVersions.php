9825f03a4c645a013a3b4fa08b2344448c470d84
Koen Eelen <koen.eelenpubliq.be>
2024-01-12 09:35:59 +0100

    Change identifier type to string to prevent problems with special chars

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20231213123216;
+    public const UDB3_CORE = 20240111123218;
     public const GEOSHAPES = 20231004135400;
 }
