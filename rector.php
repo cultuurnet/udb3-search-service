31111265fe8a671ca7039b66aa2b389bf5f26d28
Koen Eelen <koen.eelenpubliq.be>
2024-04-03 11:24:59 +0200

    Final Rector version

--- rector.php
+++ rector.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictSetUpRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -18,4 +20,18 @@ return RectorConfig::configure()
     ->withSets([SetList::TYPE_DECLARATION]) //rule to force typed declarations
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withSkip([
+        /*
+        Remove these skips once we upgrade to PHP 8 and we have a union type
+        Currently it will change code:
+            @var RegionServiceInterface|MockObject
+        to:
+            private MockObject;
+
+        breaking the code.
+        */
+        TypedPropertyFromStrictSetUpRector::class,
+        TypedPropertyFromAssignsRector::class
+        // End skipping until PHP 8
     ]);
