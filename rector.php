786188f9c89723240457c97a6a314c67cf58d0b3
Koen Eelen <koen.eelenpubliq.be>
2024-04-08 11:12:39 +0200

    revert back to old school closure syntax

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
@@ -33,6 +34,9 @@ return RectorConfig::configure()
         breaking the code.
         */
         TypedPropertyFromStrictSetUpRector::class,
-        TypedPropertyFromAssignsRector::class
+        TypedPropertyFromAssignsRector::class,
         // End skipping until PHP 8
+
+        // We don't always want short array functions because they are more difficult to read (longer line) than old school closure syntax
+        ClosureToArrowFunctionRector::class
     ]);
