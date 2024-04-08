018391bfc0c2b8e42faf74f986e6969f362e5f25
Koen Eelen <koen.eelenpubliq.be>
2024-04-08 11:17:16 +0200

    Change properties to public to make phpstan happy

--- tests/AMQP/Dummies/DummyEventNotSerializable.php
+++ tests/AMQP/Dummies/DummyEventNotSerializable.php
@@ -6,24 +6,12 @@ namespace CultuurNet\UDB3\Search\AMQP\Dummies;
 
 final class DummyEventNotSerializable
 {
-    private string $id;
-    private string $content;
+    public string $id;
+    public string $content;
 
     public function __construct(string $id, string $content)
     {
         $this->id = $id;
         $this->content = $content;
-    }
-
-    // These getters are never used, just here so phpstan is happy
-
-    public function getId(): string
-    {
-        return $this->id;
-    }
-
-    public function getContent(): string
-    {
-        return $this->content;
     }
 }
