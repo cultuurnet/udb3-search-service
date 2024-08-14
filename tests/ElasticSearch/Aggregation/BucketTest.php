edd20227c5e0e21489ade61511c322f1d6082f5b
Luc Wollants <lucwollants@gmail.com>
2024-08-14 14:55:50 +0200

    Use primitive type hinting on Bucket

--- tests/ElasticSearch/Aggregation/BucketTest.php
+++ tests/ElasticSearch/Aggregation/BucketTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\Search\ElasticSearch\Aggregation;
 
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 final class BucketTest extends TestCase
@@ -17,25 +16,5 @@ final class BucketTest extends TestCase
         $bucket = new Bucket('key', 10);
         $this->assertEquals('key', $bucket->getKey());
         $this->assertEquals(10, $bucket->getCount());
-    }
-
-    /**
-     * @test
-     */
-    public function it_checks_that_the_key_is_a_string(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Bucket key should be a string.');
-        new Bucket(true, 10);
-    }
-
-    /**
-     * @test
-     */
-    public function it_checks_that_the_count_is_an_int(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Bucket count should be an int.');
-        new Bucket('key', '10,000,0000');
     }
 }
