3778b01f1ddd06c7a1a6cb8aadb112c35d357948
Kristof Willaert <kristof@publiq.be>
2024-09-13 14:15:28 +0200

    Make appconfig and geojson-data clone directories configurable

--- docker/config.sh
+++ docker/config.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 
+APPCONFIG_ROOTDIR=${APPCONFIG:-'../appconfig'}
+GEOJSON_DATA_ROOTDIR=${GEOJSON_DATA:-'../geojson-data'}
+
 if ! grep -q "host.docker.internal" /etc/hosts; then
   echo "host.docker.internal has to be in your hosts-file, to add you need sudo privileges"
   sudo sh -c 'echo "127.0.0.1 host.docker.internal" >> /etc/hosts'
 fi
 
-DIR="../appconfig/files/udb3/docker/udb3-search-service/"
+DIR="${APPCONFIG_ROOTDIR}/files/udb3/docker/udb3-search-service/"
 if [ -d "$DIR" ]; then
   cp -R "$DIR"/* .
 else
-  echo "Error: missing appconfig see docker.md prerequisites to fix this."
+  echo "Error: missing appconfig. The appconfig repository must be cloned at ${APPCONFIG_ROOTDIR}."
   exit 1
 fi
 
-DIR="../geojson-data/output"
+DIR="${APPCONFIG_ROOTDIR}/files/udb3/docker/keys/"
+if [ -d "$DIR" ]; then
+  cp -R "$DIR"/* .
+else
+  echo "Error: missing appconfig. The appconfig repository must be cloned at ${APPCONFIG_ROOTDIR}."
+  exit 1
+fi
+
+DIR="${GEOJSON_DATA}/output"
 if [ -d "$DIR" ]; then
   cp "$DIR"/facet_mapping_regions.yml .
 else
-  echo "Error: missing geojson data see docker.md prerequisites to fix this."
-  exit 1
-fi
-
-DIR="../appconfig/files/udb3/docker/keys/"
-if [ -d "$DIR" ]; then
-  cp -R "$DIR"/* .
-else
-  echo "Error: missing appconfig see docker.md prerequisites to fix this."
+  echo "Error: missing geojson-data. The geojson-data repository must be cloned at ${GEOJSON_DATA_ROOTDIR}."
   exit 1
 fi
