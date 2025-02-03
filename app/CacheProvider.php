3785f482c98d6407cbcf73c921f9318ab11b1ff8
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-03 16:53:55 +0100

    Use CacheInterface

--- app/CacheProvider.php
+++ app/CacheProvider.php
@@ -6,18 +6,19 @@ namespace CultuurNet\UDB3\SearchService;
 
 use Predis\Client;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Contracts\Cache\CacheInterface;
 
 final class CacheProvider extends BaseServiceProvider
 {
     protected $provides = [
-        RedisAdapter::class,
+        CacheInterface::class,
     ];
 
     public function register(): void
     {
         $this->add(
-            RedisAdapter::class,
-            fn (): RedisAdapter =>
+            CacheInterface::class,
+            fn (): CacheInterface =>
             new RedisAdapter(
                 new Client(
                     $this->parameter('cache.redis')
