fd12cc7d49e6720adbbb73d65791d8d8c39bc741
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2025-02-03 16:49:26 +0100

    Add unit test

---
+++ tests/Http/Authentication/Access/CultureFeedConsumerProviderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CultuurNet\UDB3\Search\Http\Authentication\Access;
+
+use PHPUnit\Framework\TestCase;
+
+final class CultureFeedConsumerProviderTest extends TestCase
+{
+
+}
