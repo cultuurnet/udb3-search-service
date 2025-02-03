1b7346592279e0c7a32421813c3d9e9537a85393
Koen Eelen <koen.eelenpubliq.be>
2025-02-03 10:25:53 +0100

    Improve error handeling when file does not exist to read

--- src/FileReader.php
+++ src/FileReader.php
@@ -8,7 +8,12 @@ final class FileReader
 {
     public static function read(string $filepath): string
     {
-        $content = file_get_contents($filepath);
+        try {
+            $content = file_get_contents($filepath);
+        }
+        catch (\Exception $e) {
+            $content = false;
+        }
 
         if ($content === false) {
             throw new \RuntimeException('Failed to read file ' . $filepath);
