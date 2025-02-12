4ae009b278c8da65f844cd971a286e7ff100b7b5
steftrenson <steftrenson@gmail.com>
2025-02-12 13:09:34 +0100

    CacheInterface does not need to be provided anymore

--- app/CacheProvider.php
+++ app/CacheProvider.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace CultuurNet\UDB3\SearchService;
 
 use Predis\Client;
-use Symfony\Component\Cache\Adapter\RedisAdapter;
-use Symfony\Contracts\Cache\CacheInterface;
 
 final class CacheProvider extends BaseServiceProvider
 {
     protected $provides = [
         Client::class,
-        CacheInterface::class,
     ];
 
     public function register(): void
@@ -20,15 +17,5 @@ final class CacheProvider extends BaseServiceProvider
         $this->add(Client::class, fn (): Client => new Client(
             $this->parameter('cache.redis')
         ));
-
-        $this->add(
-            CacheInterface::class,
-            fn (): CacheInterface =>
-            new RedisAdapter(
-                $this->get(Client::class),
-                'permission',
-                86400,
-            ),
-        );
     }
 }
