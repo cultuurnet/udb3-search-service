8eb7b6590a54645aa43723b43b418c721a1685bc
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-04-07 13:33:53 +0200

    wire caching

--- app/Taxonomy/TaxonomyServiceProvider.php
+++ app/Taxonomy/TaxonomyServiceProvider.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\SearchService\Taxonomy;
 
+use CultuurNet\UDB3\Search\Cache\CacheFactory;
+use CultuurNet\UDB3\Search\Taxonomy\CachedTaxonomyApiClient;
 use CultuurNet\UDB3\Search\Taxonomy\JsonTaxonomyApiClient;
 use CultuurNet\UDB3\Search\Taxonomy\TaxonomyApiClient;
 use CultuurNet\UDB3\SearchService\BaseServiceProvider;
 use CultuurNet\UDB3\SearchService\Error\LoggerFactory;
 use CultuurNet\UDB3\SearchService\Error\LoggerName;
 use GuzzleHttp\Client;
+use Predis\Client as PredisClient;
 
 final class TaxonomyServiceProvider extends BaseServiceProvider
 {
@@ -27,6 +30,25 @@ final class TaxonomyServiceProvider extends BaseServiceProvider
                 LoggerFactory::create(
                     $this->getContainer(),
                     LoggerName::forWeb()
+                )
+            )
+        );
+
+        $this->addShared(
+            CachedTaxonomyApiClient::class,
+            fn (): TaxonomyApiClient => new CachedTaxonomyApiClient(
+                CacheFactory::create(
+                    $this->container->get(PredisClient::class),
+                    'taxonomy',
+                    86400 // one day
+                ),
+                new JsonTaxonomyApiClient(
+                    new Client(),
+                    $this->parameter('taxonomy.terms'),
+                    LoggerFactory::create(
+                        $this->getContainer(),
+                        LoggerName::forWeb()
+                    )
                 )
             )
         );
