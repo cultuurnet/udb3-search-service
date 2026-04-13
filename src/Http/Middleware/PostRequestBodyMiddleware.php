590aa0ddeffc2fd204b71c4130ab43e34c41450a
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-04-13 13:57:50 +0200

    refactor check authentication params

--- src/Http/Middleware/PostRequestBodyMiddleware.php
+++ src/Http/Middleware/PostRequestBodyMiddleware.php
@@ -19,7 +19,7 @@ final class PostRequestBodyMiddleware implements MiddlewareInterface
             return $handler->handle($request);
         }
 
-        if (!empty($this->stripAuthenticationParams($request->getQueryParams()))) {
+        if ($this->hasNonAuthenticationParams($request->getQueryParams())) {
             return (
                 new BadRequest('POST requests do not allow query parameters in the URL. Use the request body instead.')
             )->toResponse();
@@ -41,18 +41,13 @@ final class PostRequestBodyMiddleware implements MiddlewareInterface
         return $handler->handle($request);
     }
 
-    private function stripAuthenticationParams(array $queryParams): array
+    private function hasNonAuthenticationParams(array $queryParams): bool
     {
-        // Use array_diff_key to create a diff between the given query params (keys) and the authentication-
-        // specific query params (keys). The result is either an empty list if no other query params are
-        // included in the request, or a list of (unsupported) query params.
-        // Since array_diff_key creates a diff based on array keys, we use array_flip to create an array with
-        // authentication-specific keys like clientId and apiKey.
-        // E.g. ['clientId', 'apiKey'] becomes ['clientId' => 0, 'apiKey' => 1]
-        // (the values don't really matter in this case)
-        return array_diff_key(
+        $nonAuthenticationParams = array_filter(
             $queryParams,
-            array_flip(['clientId', 'apiKey'])
+            fn (string $key) => !in_array($key, ['clientId', 'apiKey']),
+            ARRAY_FILTER_USE_KEY
         );
+        return !empty($nonAuthenticationParams);
     }
 }
