43fb8407080a7c5308f42ddeecfc8aeadfa4dfd0
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-07-15 13:52:10 +0200

    update schemaversions trigger reindex

--- src/ElasticSearch/Operations/SchemaVersions.php
+++ src/ElasticSearch/Operations/SchemaVersions.php
@@ -6,6 +6,6 @@ namespace CultuurNet\UDB3\Search\ElasticSearch\Operations;
 
 final class SchemaVersions
 {
-    public const UDB3_CORE = 20260701173900;
+    public const UDB3_CORE = 20260715115100;
     public const GEOSHAPES = 20250101000000;
 }
