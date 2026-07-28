20f48d2fb158a9eef62e44e52f0302a0a56259e7
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2026-07-28 16:46:03 +0200

    remove strtolower()

--- src/ElasticSearch/JsonDocument/Properties/Url.php
+++ src/ElasticSearch/JsonDocument/Properties/Url.php
@@ -51,7 +51,7 @@ final class Url
 
     public function getDomain(): string
     {
-        $host = strtolower($this->urlParts['host']);
+        $host = $this->urlParts['host'];
 
         if (strpos($host, 'www.') === 0) {
             return substr($host, strlen('www.'));
